ebd1dec1f2741a03b10e191854c6ae18b777a715
Tharindu Epasingha <2020cs050@stu.ucsc.cmb.ac.lk>
2022-11-10 14:31:14 +0530

    service provider page added

--- php/service_provider_home.php
+++ php/service_provider_home.php
@@ -1,27 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link rel="stylesheet" href="../styles/form.css">
-    <link rel="stylesheet" href="../styles/login.css">
-
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@500&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <!-- <script src="https://kit.fontawesome.com/a076d05399.js" ></script> -->
-    <script src="https://kit.fontawesome.com/128d66c486.js" crossorigin="anonymous"></script>
-    <title>AUDEX</title>
-</head>
-<body>
-
+<?php require('./header.php') ?>
 <nav>
     <input type="checkbox" name="check" id="check" onchange="docheck()">
     <label for="check" class="checkbtn">
         <i class="fas fa-bars"></i>
     </label>
-    <img src="img/image 1.png" alt="">
+    <img src="../img/image 1.png" alt="">
     <ul>
         <li><a href="#" class="nav_tags">Home</a></li>
         <li><a href="#" class="nav_tags">Shop</a></li>
@@ -40,3 +24,4 @@
         <a href="#"> <i class="fa fa-comments"></i><span>Messages</span></a>       
     </div>
     
+    <?php require('./footer.php') ?>
