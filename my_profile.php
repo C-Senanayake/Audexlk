af1b92b5d766a0f041c8c9f8d1b5b8c148250005
dineshWic <109039671+dineshWic@users.noreply.github.com>
2022-11-10 22:36:59 +0530

    buyer my profile done

--- my_profile.php
+++ my_profile.php
@@ -17,11 +17,11 @@
 
 
     <div class="sidebar">
-        <a href="#"><i class="fas fa-qrcode"></i> <span>My Profile</span></a>
-        <a href="#"> <i class="fa fa-cog" aria-hidden="true"></i><span>Watch List</span></a>
-        <a href="#"> <i class="fa fa-ad" aria-hidden="true"></i><span>Feedback</span></a>
-        <a href="#"> <i class="fa fa-calendar" aria-hidden="true"></i><span>Reactions</span></a>
-        <a href="#"> <i class="fa fa-comments"></i><span>Messages</span></a>       
+        <a href="#" id="now"><i class="fas fa-address-card"></i> <span>My Profile</span></a>
+        <a href="#"> <i class="far fa-calendar-check" aria-hidden="true"></i><span>Watch List</span></a>
+        <a href="#"> <i class="fa fa-comments-o" aria-hidden="true"></i><span>Feedback</span></a>
+        <a href="#"> <i class="fa fa-thumbs-up" aria-hidden="true"></i><span>Reactions</span></a>
+        <a href="#"> <i class="fa fa-envelope"></i><span>Messages</span></a>       
     </div>
     <div class="content-box">
         <div class="name-field">
@@ -30,14 +30,48 @@
         </div>
         <div class="details">
             <div class="details-description">
-                <h3>Who I Am?</h3>
+                <h3>Who I Am? Dinesh</h3>
                 <p>
                 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque delectus voluptatibus odit 
                 corrupti mollitia sint libero? Nobis provident qui placeat autem magni enim! Corporis nobis, odio tempore itaque esse iure.
                 </p>
             </div>
             <div class="details-info">
-                details-info
+                <table>
+                    <tr>
+                        <th>First Name:</th>
+                        <td>Tharindu</td>
+                    </tr>
+                    <tr>
+                        <th>Last Name:</th>
+                        <td>Epasinghe</td>
+                    </tr>
+                    <tr>
+                        <th>Email:</th>
+                        <td>tharindu@gmail.com</td>
+                    </tr>
+                    <tr>
+                        <th>Password:</th>
+                        <td>dnskln</td>
+                    </tr>
+                    <tr>
+                        <th>Address Line 1:</th>
+                        <td>Wickrama Stores, Dolamulla</td>
+                    </tr>
+                    <tr>
+                        <th>Address Line 2:</th>
+                        <td>Maliduwa, Akuressa</td>
+                    </tr>
+                    <tr>
+                        <th>Mobile:</th>
+                        <td>0763183464</td>
+                    </tr>
+                    <tr>
+                        <th>User Type:</th>
+                        <td>Buyer</td>
+                    </tr>
+                </table>
+                <button type="submit">Edit Details</button>
             </div>
         </div>
 
