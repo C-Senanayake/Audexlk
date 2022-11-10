5eeb0690438922577ec627bee030c8d271331fc1
Chamath Senanayake <2020cs171@stu.ucsc.cmb.ac.lk>
2022-11-10 15:31:44 +0530

    Created seller_dashboard part

--- php/seller_dashboard.php
+++ php/seller_dashboard.php
@@ -2,14 +2,6 @@
 <?php require_once('../include/connection.php');?>
 <?php require_once('../include/functions.php');?>
 
-<?php 
-    //checking if a user is logged in
-    if(!isset($_SESSION['user_id'])){
-        header('Location:index.php');
-    }
-?>
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -17,7 +9,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../styles/form.css">
-    <link rel="stylesheet" href="../styles/login.css">
+    <link rel="stylesheet" href="../styles/sidebar.css">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@500&display=swap" rel="stylesheet">
     <!-- <script src="https://kit.fontawesome.com/a076d05399.js" ></script> -->
     <script src="https://kit.fontawesome.com/128d66c486.js" crossorigin="anonymous"></script>
@@ -35,15 +27,39 @@
             <li><a href="#" class="nav_tags">Shop</a></li>
             <li><a href="#" class="nav_tags">Sound Engineers</a></li>
             <li><a href="#" class="nav_tags">Events</a></li>
-            <li><a href="logout.php" class="nav_tags">Hi <?php echo $_SESSION['first_name'];?>
-</a></li>
+            <li><a href="#" class="nav_tags">Login</a></li>
         </ul>
     </nav>
     <div class="container">
-        
+        <div class="sidebar">
+            <div class="side">
+                <i class="fas fa-qrcode"></i>
+                <a href="#">Dashboard</a>
+            </div>
+            <div class="side">
+                <i class="fa fa-cog"></i>
+                <a href="#">Profile Settings</a>
+
+            </div>
+            <div class="side">
+                <i class="fa fa-ad" ></i>
+                <a href="#">Advertisements</a>
+            </div>
+            <div class="side">
+                <i class="fa fa-calendar" ></i>
+                <a href="#">Sell Item</a>
+
+            </div>
+            <div class="side">
+                <i class="fa fa-comments"></i>
+                <a href="#">Messages</a>
+
+            </div>
+        </div>
     </div>
 </body>
 <script src="../js/form.js"></script>
 </html>
 <?php mysqli_close($connection);?>
-<!-- Closing the connection
+<!-- Closing the connection-->
+
