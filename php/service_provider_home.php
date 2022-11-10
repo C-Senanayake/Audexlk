ed96c4ce3c19f318b09de905146241b91e40100c
Tharindu Epasingha <2020cs050@stu.ucsc.cmb.ac.lk>
2022-11-10 14:25:23 +0530

    minor changes

--- php/service_provider_home.php
+++ php/service_provider_home.php
@@ -1,4 +1,20 @@
-<?php include('./php/header.php.php') ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="../styles/form.css">
+    <link rel="stylesheet" href="../styles/login.css">
+
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@500&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- <script src="https://kit.fontawesome.com/a076d05399.js" ></script> -->
+    <script src="https://kit.fontawesome.com/128d66c486.js" crossorigin="anonymous"></script>
+    <title>AUDEX</title>
+</head>
+<body>
 
 <nav>
     <input type="checkbox" name="check" id="check" onchange="docheck()">
@@ -24,4 +40,3 @@
         <a href="#"> <i class="fa fa-comments"></i><span>Messages</span></a>       
     </div>
     
-<?php include('./php/footer.php') ?>
