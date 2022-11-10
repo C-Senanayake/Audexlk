0a077250f340647d65bdc421ba122267116b93d7
dineshWic <109039671+dineshWic@users.noreply.github.com>
2022-11-10 18:01:43 +0530

    my profile creation

--- my_profile.php
+++ my_profile.php
@@ -17,11 +17,31 @@
 
 
     <div class="sidebar">
-        <a href="#"><i class="fas fa-qrcode"></i> <span>Dashboard</span></a>
-        <a href="#"> <i class="fa fa-cog" aria-hidden="true"></i><span>Profile Settings</span></a>
-        <a href="#"> <i class="fa fa-ad" aria-hidden="true"></i><span>Advertisements</span></a>
-        <a href="#"> <i class="fa fa-calendar" aria-hidden="true"></i><span>Calender</span></a>
+        <a href="#"><i class="fas fa-qrcode"></i> <span>My Profile</span></a>
+        <a href="#"> <i class="fa fa-cog" aria-hidden="true"></i><span>Watch List</span></a>
+        <a href="#"> <i class="fa fa-ad" aria-hidden="true"></i><span>Feedback</span></a>
+        <a href="#"> <i class="fa fa-calendar" aria-hidden="true"></i><span>Reactions</span></a>
         <a href="#"> <i class="fa fa-comments"></i><span>Messages</span></a>       
+    </div>
+    <div class="content-box">
+        <div class="name-field">
+            <img src="img/icons/healthicons_ui-user-profile.png" alt="profile_pic" srcset="">
+            <h1>Mr. User Name</h1>
+        </div>
+        <div class="details">
+            <div class="details-description">
+                <h3>Who I Am?</h3>
+                <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque delectus voluptatibus odit 
+                corrupti mollitia sint libero? Nobis provident qui placeat autem magni enim! Corporis nobis, odio tempore itaque esse iure.
+                </p>
+            </div>
+            <div class="details-info">
+                details-info
+            </div>
+        </div>
+
+
     </div>
     
 
